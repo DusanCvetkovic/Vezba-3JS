023dc8582bd076aaa27b90892b3abc5d79e98c60
DusanCvetkovic <71986600+DusanCvetkovic@users.noreply.github.com>
2021-02-09 21:50:38 +0100

    Add files via upload

---
+++ Script3.js
@@ -0,0 +1,22 @@
+const fruits = ['banana', 'orange', 'apple'];
+
+fruits[3] = 'grapes';
+
+console.log(fruits);
+
+fruits.push('mango');
+
+fruits.unshift('strawberries');
+
+fruits.pop();
+
+console.log(Array.isArray('hello'));
+
+console.log(fruits.indexOf('oranges'));
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+
+
